3425f35e1e5b484e652c9b0bdc2cebf0a1a8d3cc
Nguyễn Như Quỳnh <86521181+quynhniee@users.noreply.github.com>
2022-12-07 21:11:26 +0700

    7/12/2022

--- Thiet_ke_dap_thuy_dien.py
+++ Thiet_ke_dap_thuy_dien.py
@@ -1,27 +1,33 @@
-# IR :))
+import re
+from bisect import bisect_left
+from sys import stdin
 
-for __ in range(int(input())):
-    n = int(input()) # so luong dap
-    p = [int(i) for i in input().split()] # vi tri dap thu i
-    h = [int(i) for i in input().split()] # chieu cao cua dap thu i
+# test cho input như cdb :))
+list = iter([int(i) for i in re.split('\\s+', ' '.join(stdin.readlines()).strip())])
+
+for __ in range(next(list)):
+    n = next(list) # so luong dap
+    p = [next(list) for i in range(n)] # vi tri dap thu i
+    h = [next(list) for i in range(n)] # chieu cao cua dap thu i
+
     stack = []
     higher = [] # vi tri dap dau tien ben trai cao hon dap thu i
     for i in range(n):
-        while len(stack) > 0 and h[i] >= h[stack[-1]]:
+        while stack and h[i] >= h[stack[-1]]:
             stack.pop()
-        higher.append(0 if not len(stack) else stack[-1])
+        higher.append(-1 if not stack else stack[-1])
         stack.append(i)
 
-    v = [0]*n  # the tich lon nhat co the chua den dap thu i
-    v[0] = p[0]*h[0]
+    sum = [0] * n   # tong the tich cac dap truoc i
     for i in range(1, n):
-        v[i] = v[higher[i]] + (p[i] - 1 - p[higher[i]]) * h[i] - sum(h[p[higher[i]]+1:p[i]], 0) if higher[i] > 0 else p[i] * h[i] - sum(h[:i])
+        sum[i] = sum[i-1] + h[i-1]
 
-    for q in range(int(input())):
-        k = int(input())
-        if k <= v[n-1]:
-            res = 0
-            while v[res] < k and res < n-1:
-                res += 1
-            print(res)
-        else:   print('Ngap')
+    v = [0] * n  # the tich lon nhat co the chua den dap thu i
+    for i in range(n):
+        v[i] = v[higher[i]] + (p[i] - 1 - p[higher[i]]) * h[i] - (sum[i] - sum[higher[i]+1]) if higher[i] != -1 else p[i] * h[i] - sum[i]
+
+    for q in range(next(list)):
+        k = next(list)
+        res = 0
+        print(bisect_left(v, k, 0, n))
+        
