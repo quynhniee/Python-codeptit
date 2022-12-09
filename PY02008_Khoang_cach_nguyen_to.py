9ba7318eb3296c0b9f30726282811c844bc39dbe
Nguyễn Như Quỳnh <86521181+quynhniee@users.noreply.github.com>
2022-12-09 23:57:33 +0700

    9/12/2022

--- PY02008_Khoang_cach_nguyen_to.py
+++ PY02008_Khoang_cach_nguyen_to.py
@@ -1,7 +1,6 @@
-# p = [False]*100000
 max_val = 100000
 p = [True for i in range(max_val+1)]
-def seivePrime ():
+def sievePrime ():
     p[0], p[1] = False, False
     i = 2
     while (i*i <= max_val):
@@ -11,18 +10,12 @@ def seivePrime ():
         i += 1
             
 n, x = map(int, input().strip().split())
-seivePrime()
+sievePrime()
 res = [x]
-def primeList ():
-    cou, i = 0, 0
-    while cou < n:
-        if p[i]:    
-            res.append(i + res[len(res) - 1])
-            cou += 1
-        i +=1
-    for k in res:
-        print(k, end=" ")
-    print()
-primeList()
-        
-    
+cou, i = 0, 0
+while cou < n:
+    if p[i]:    
+        res.append(i + res[-1])
+        cou += 1
+    i +=1
+print(*res)    
