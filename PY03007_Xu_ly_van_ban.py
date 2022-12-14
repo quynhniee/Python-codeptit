cf42730100508d1b0e5d00a1cfcbcafc13465866
Nguyễn Như Quỳnh <86521181+quynhniee@users.noreply.github.com>
2022-12-14 20:49:10 +0700

    14/12/2022

--- PY03007_Xu_ly_van_ban.py
+++ PY03007_Xu_ly_van_ban.py
@@ -8,3 +8,9 @@ for line in inputString:
 for i in (re.split('\n\s?', res)):
     if i != '':
         print(i[0:1].upper() + i[1:])
+
+# Cach 2
+# s = ''.join(stdin.readlines()).lower()
+# s = re.sub('\\s+', ' ', s)
+# s = re.split('[\\?\\.\\!] ?', s)
+# print(*[i.capitalize() for i in s if i != ''], sep='\n')
--- PYKT072_Xoay_vong_xau_ky_tu.py
+++ PYKT072_Xoay_vong_xau_ky_tu.py
@@ -1,19 +1,18 @@
-a, n = [], int(input())
-for i in range(n):
-    a += [input()]
-s = a[0]
-ok, ans = 1, int(1e9)
-for i in range(len(s)):
-    d = 0
-    for j in range(n):
-        x = a[j]
-        for k in range(len(s)):
-            if x == s:
-                d += k
-                break
-            x = x[1:] + x[0]
-        if x != s:  ok = 0
-    ans = min(ans, d)
-    s = s[1:] + s[0]
-print(-1 if not ok else ans)
+res, n, isValid = 1e9, int(input()), True
+list = [input() for i in range(n)]
 
+for s0 in list:
+    for i in range(len(s0)):
+        d = 0
+        for j in list:
+            s = j
+            for k in range(len(j)):
+                if s0 == s:
+                    d += k
+                    break
+                s = s[1:] + s[0]
+            if s0 != s:
+                isValid = False
+        res = min(res, d)
+
+print(-1 if isValid == False else res)
